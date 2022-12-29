9d724eef8358cb225c79fd0d36d4719a55f77a9d
Julian Cutajar <julian@thewknd.io>
2022-12-29 11:11:48 +0100

    Head Tag

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>DJ Events</title>
+        <meta name="description" content="Welcome to DJ Events" />
+      </Head>
+
       <h1>Home</h1>
     </div>
   );
