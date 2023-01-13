b26770dd404d1136e064f2fe9cf8f1a478cdaef0
Julian Cutajar <julian@thewknd.io>
2023-01-13 08:56:04 +0100

    Add getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+// Configurations
+import { API_URL } from "@/config/index";
+
 // Import Layout
 import Layout from "@/components/Layout";
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1
+  };
 }
